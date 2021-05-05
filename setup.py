c7abeb649a2dd118aa185665338ebddbf793573d
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2021-05-06 00:23:27 +0800

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
  
 setup(name='pyAudioKits',
-      version="0.1",
-      description='A SDK for Event Cameras',
+      version="0.2",
+      description='Powerful Python audio workflow support based on librosa and other libraries',
       author='HarmoniaLeo',
       author_email='1532282692@qq.com',
       maintainer='HarmoniaLeo',
       maintainer_email='1532282692@qq.com',
-      packages=['pyAudioKits.filters','pyAudioKits.freqDomainAnalyse','pyAudioKits.timeDomainAnalyse','pyAudioKits.audio','pyAudioKits.modelBasedAnalyse'],
+      packages=['pyAudioKits'],
       license="Public domain",
       platforms=["any"],
      )
